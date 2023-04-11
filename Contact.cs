4ac778a8fbd99a03403aea0ca5edc0e8df19549b
samirasingh19 <samira.singh021@gmail.com>
2023-04-11 19:10:38 +0530

    Contact.cs

---
+++ Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProblem
+{
+    public class Contact
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+
+        public int zip { get; set; }
+        public int phoneNumber { get; set; }
+        public string eMail { get; set; }
+
+    }
+}
